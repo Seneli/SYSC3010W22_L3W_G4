4ed648c3aff129e6df6b95335918d7ea575bf574
seneli <seneli.sene@gmail.com>
2022-02-26 12:19:46 -0500

    failed attempt to change favicon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { SignIn, Register, Screening, Mask, Temperature, Success, Error } from "./pages"; 
 
 interface AppProps {
-    
 }
  
 const App : React.FunctionComponent<AppProps> = () => {
