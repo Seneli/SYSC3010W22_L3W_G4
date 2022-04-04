394bb783f5f5e423408884342f13ac9e7cde57ec
seneli <seneli.sene@gmail.com>
2022-04-04 17:15:51 -0400

    deleteimages worked

--- client/src/firebase/initFirebase.ts
+++ client/src/firebase/initFirebase.ts
@@ -1,7 +1,5 @@
-//import firebase from "firebase/app";
 import { initializeApp } from 'firebase/app';
 import { getStorage, ref, deleteObject } from 'firebase/storage';
-//import "firebase/auth"; //if we want auth we need this
 import { getDatabase } from 'firebase/database';
 
 const config = {
@@ -12,22 +10,12 @@ const config = {
     projectId: process.env.REACT_APP_PUBLIC_FIREBASE_PROJECT_ID
 };
 
-const storageConfig = {
-    apiKey: process.env.REACT_APP_PUBLIC_FIREBASE_API_KEY,
-    authDomain: `${process.env.REACT_APP_PUBLIC_FIREBASE_PROJECT_ID}.firebaseapp.com`,
-    databaseURL: `https://${process.env.REACT_APP_PUBLIC_FIREBASE_PROJECT_ID}-default-rtdb.firebaseio.com`,
-    storageBucket: `gs://covid-rapid-screener.appspot.com`
-};
-
 const app = initializeApp(config);
-//const storageApp = initializeApp(storageConfig);
 const realtimeDB = getDatabase(app);
-
 const firebaseStorage = getStorage(app);
-//const firebaseStorage = getStorage(storageApp);
 
 const deleteImage = (refName: string) => {
-    const imageRef = ref(firebaseStorage, process.env.REACT_APP_SYSTEM_NUMBER + '/' + refName);
+    const imageRef = ref(firebaseStorage, process.env.REACT_APP_PUBLIC_FIREBASE_SYSTEM_NUMBER + '/' + refName);
     console.log(imageRef);
 
     deleteObject(imageRef)
